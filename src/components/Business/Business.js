6c74a1ba1f74611c904cf0e90d50aee2bc867d86
SorinGabriel02 <lupu_sg@yahoo.com>
2019-09-12 18:57:25 +0300

    Works

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -7,7 +7,7 @@ class Business extends React.Component {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={business.imageSrc} alt="" />
+          <img src={business.imageSrc} alt="Image of business" />
         </div>
         <h2>{business.name}</h2>
         <div className="Business-information">
@@ -21,8 +21,8 @@ class Business extends React.Component {
           </div>
           <div className="Business-reviews">
             <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating}</h3>
-            <p>{business.reviewCount}</p>
+            <h3 className="rating">Rating: {business.rating}</h3>
+            <p>{business.reviewCount} reviews</p>
           </div>
         </div>
       </div>
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -2,35 +2,31 @@ const apiKey =
   "FFvh-U80MrPbE5T-_an2Ouk34zf-qAFNxFuSiDHs-D5mZ3p-tg2sNjcPt_jdqfdfH3s1ecxeVbvbCgQSvjlPPL1r40HKdYAX21kTCxVVMNKN6rm2AAp6Xbuzm1h5XXYx";
 
 const Yelp = {
-  search(term, location, sortBy) {
-    return fetch(
+  async search(term, location, sortBy) {
+    const response = await fetch(
       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
       {
         headers: { Authorization: `Bearer ${apiKey}` }
       }
-    )
-      .then(response => {
-        return response.json();
-      })
-      .then(jsonResponse => {
-        if (jsonResponse.businesses) {
-          return jsonResponse.businesses.map(business => {
-            console.log(business);
-            return {
-              id: business.id,
-              imageSrc: business.image_url,
-              name: business.name,
-              adress: business.location.adress1,
-              city: business.location.city,
-              state: business.location.state,
-              zipCode: business.location.postal_code,
-              category: business.categories[0].title,
-              rating: business.rating,
-              reviewCount: business.review_count
-            };
-          });
-        }
+    );
+    const jsonResponse = await response.json();
+    if (jsonResponse.businesses) {
+      return jsonResponse.businesses.map(business => {
+        console.log(business);
+        return {
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          adress: business.location.adress1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.postal_code,
+          category: business.categories[0].title,
+          rating: business.rating,
+          reviewCount: business.review_count
+        };
       });
+    }
   }
 };
 
