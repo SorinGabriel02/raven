47c7e521459d6cef8de8353a53a1c354b4759d05
SorinGabriel02 <lupu_sg@yahoo.com>
2020-09-04 13:56:53 +0300

    Updates

---
+++ precache-manifest.12acc772fcfec6d70dc08a7d127d873b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db3a07e9f07249ea024b8e1344f34cf2",
+    "url": "/raven/index.html"
+  },
+  {
+    "revision": "0d984dc56b4372135921",
+    "url": "/raven/static/css/main.1e846325.chunk.css"
+  },
+  {
+    "revision": "5eacca09df00d3ec6f03",
+    "url": "/raven/static/js/2.6f1a54a5.chunk.js"
+  },
+  {
+    "revision": "0d984dc56b4372135921",
+    "url": "/raven/static/js/main.100b09e9.chunk.js"
+  },
+  {
+    "revision": "444fa4cc0f7072f7cf0a",
+    "url": "/raven/static/js/runtime~main.48f65f0b.js"
+  },
+  {
+    "revision": "7bcd820171a58a57c46c810bda256ba4",
+    "url": "/raven/static/media/background_search_desktop.7bcd8201.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/raven/precache-manifest.65b09be762c6a53df9fa349e39cb96f7.js"
+  "/raven/precache-manifest.12acc772fcfec6d70dc08a7d127d873b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/raven/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
